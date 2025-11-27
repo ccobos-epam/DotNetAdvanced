a51e202c50b0198ba0ede35cfa3702abfba6dc5e
Ciro Cobos <ciro_cobos@epam.com>
2025-11-26 21:21:58 -0500

    added v1 specs for swagger

--- LayeredArchitectures/Catalog/Code/CatalogService/Program.cs
+++ LayeredArchitectures/Catalog/Code/CatalogService/Program.cs
@@ -3,12 +3,13 @@ using DataAccess;
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using Microsoft.EntityFrameworkCore;
+using NSwag.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>((serviceProvider, options) => 
 {
@@ -29,9 +30,11 @@ builder.Services
     {
         o.MaxEndpointVersion = 1;
         o.MinEndpointVersion = 1;
+        o.FlattenSchema = false;
+        o.ShortSchemaNames = false;
         o.DocumentSettings = s =>
         {
-            s.DocumentName = "Initial Release";
+            s.DocumentName = "v1";
             s.Title = "Catalog API";
             s.Version = "v1";
         };
@@ -40,10 +43,10 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
 
 Action<Config> FEConfig = options =>
 {
@@ -51,9 +54,20 @@ Action<Config> FEConfig = options =>
     options.Versioning.PrependToRoute = false;
     options.Versioning.DefaultVersion = 1;
 };
+Action<OpenApiDocumentMiddlewareSettings> FEOpenApi = options =>
+{
+    options.Path = "/apiSpecs/specs.json";
+    options.DocumentName = "v1";
+};
+Action<SwaggerUiSettings> FESwagger = options =>
+{
+    options.DocExpansion = "full";
+    options.DocumentPath = "/apiSpecs/specs.json";
+    options.Path = "/docs";
+};
 app
     .UseFastEndpoints(FEConfig)
-    .UseSwaggerGen();
+    .UseSwaggerGen(FEOpenApi, FESwagger);
 app.Run();
 
 public partial class Program { }
