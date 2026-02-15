9406a232eade78d23f466901821a8794b0e87912
Ciro Cobos <ciro_cobos@epam.com>
2026-02-14 20:22:04 -0500

    defined models for the cart project

---
+++ LayeredArchitectures/SV01/CartProject/Source/WebApi/Features/Cart/Entities/Cart.cs
@@ -0,0 +1,7 @@
+﻿namespace CartWebApi.Features.Cart.Entities;
+
+public class Cart
+{
+  public required Guid  CartId { get; set; } = Guid.NewGuid();
+  public List<Product> CartItems { get; set; } = [];
+}
---
+++ LayeredArchitectures/SV01/CartProject/Source/WebApi/Features/Cart/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CartWebApi.Features.Cart.Entities;
+
+public class Product
+{
+  public required Guid Id { get; set; }
+  public required string Name { get; set; }
+  public string? ImageUrl { get; set; }
+  public string? ImageAltText { get; set; }
+  public required decimal Price { get; set; }
+  public required int Quantity { get; set; } = 1;
+}
