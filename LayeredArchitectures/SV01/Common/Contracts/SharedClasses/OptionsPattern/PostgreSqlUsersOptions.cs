ab84b25b96b91f0a3b811a8139eb0b786b9349ab
Ciro Cobos <ciro_cobos@epam.com>
2026-02-17 11:47:46 -0500

    added possible config values for postgre sql

---
+++ LayeredArchitectures/SV01/Common/Contracts/SharedClasses/OptionsPattern/PostgreSqlUsersOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedClasses.OptionsPattern;
+
+public class PostgreSqlUsersOptions
+{
+  private const string BasePath = "PostgreSqlUsers";
+  public const string AdminPath = $"{BasePath}/Admin";
+  public const string DocumentsPath = $"{BasePath}/DocumentsDb";
+  public const string RelationalPath = $"{BasePath}/RelationalDb";
+  public const string UsersPath = $"{BasePath}/UserManagement";
+  
+  public string UserId { get; set; } = null!;
+  public string Password { get; set; } = null!;
+  public string Schema { get; set; } = null!;
+}
