b0bc6015109a0f11d07198805da27995b4b3cb66
Ciro Cobos <ciro_cobos@epam.com>
2026-02-17 11:46:53 -0500

    added base class library for common classes

---
+++ LayeredArchitectures/SV01/Common/Contracts/SharedClasses/OptionsPattern/PostgreSqlInstanceOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedClasses.OptionsPattern;
+
+public class PostgreSqlInstanceOptions
+{
+  public string Host { get; set; } = null!;
+  public int Port { get; set; }
+  public string Database { get; set; }  = null!;
+}
